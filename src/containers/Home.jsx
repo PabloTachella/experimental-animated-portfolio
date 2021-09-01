32320889ddf4d054bddf18e75af2f44fd6feb9d6
Pablo <pablotachella95@gmail.com>
2021-09-01 20:16:10 -0300

    Se refactorizo el codigo haciendolo mas legible y se agrego una nueva seccion 'sobre mi' que falta maquetar y estilizar

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Presentation from '../components/Presentation'
-import Animation from '../components/Animation'
+import Main from '../components/Main'
 import '../assets/styles/App.css'
 
 const Home = ({ startAnimation }) => {
     return (
         <>
             {startAnimation === false && <Presentation />}
-            {startAnimation === true && <Animation />}
+            {startAnimation === true && <Main />}
         </>
     )
 
